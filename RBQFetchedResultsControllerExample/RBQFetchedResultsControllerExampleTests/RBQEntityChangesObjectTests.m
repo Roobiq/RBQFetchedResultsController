8dfe4ffb0458de01c565d3eb311ca3d3bd8650fb
Adam Fish <adam@roobiq.com>
2015-05-29 19:29:29 -0700

    Fixing formatting style in RBQEntityChangesObjectTests
    Also added clang ignore for the performSelector calls.

--- RBQFetchedResultsControllerExample/RBQFetchedResultsControllerExampleTests/RBQEntityChangesObjectTests.m
+++ RBQFetchedResultsControllerExample/RBQFetchedResultsControllerExampleTests/RBQEntityChangesObjectTests.m
@@ -17,62 +17,97 @@
 
 @implementation RBQEntityChangesObjectTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 #pragma mark - Utility
 
-- (RBQEntityChangesObject *)createEntityChangesObject {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+
+- (RBQEntityChangesObject *)createEntityChangesObject
+{
     SEL selector = NSSelectorFromString(@"createEntityChangeObjectWithClassName:");
+    
     RBQEntityChangesObject *entityChangesObject = [[RBQEntityChangesObject class] performSelector:selector withObject:@"TestObject"];
+    
     return entityChangesObject;
 }
 
 #pragma mark - Test Case
 
-- (void)testInitializeEntityChangesObject {
+- (void)testInitializeEntityChangesObject
+{
     RBQEntityChangesObject *entityChangesObject = [self createEntityChangesObject];
+    
     XCTAssert([entityChangesObject.className isEqualToString:@"TestObject"]);
     XCTAssert([entityChangesObject.addedSafeObjects isKindOfClass:[NSSet class]]);
     XCTAssert([entityChangesObject.deletedSafeObjects isKindOfClass:[NSSet class]]);
     XCTAssert([entityChangesObject.changedSafeObjects isKindOfClass:[NSSet class]]);
 }
 
-- (void)testDidAddSafeObject {
+- (void)testDidAddSafeObject
+{
     RBQEntityChangesObject *entityChangesObject = [self createEntityChangesObject];
+    
     SEL selector = NSSelectorFromString(@"didAddSafeObject:");
-    RBQSafeRealmObject *safeObject = [[RBQSafeRealmObject alloc] initWithClassName:@"TestObject" primaryKeyValue:@"key" primaryKeyType:RLMPropertyTypeString realm:[RLMRealm defaultRealm]];
+    
+    RBQSafeRealmObject *safeObject = [[RBQSafeRealmObject alloc] initWithClassName:@"TestObject"
+                                                                   primaryKeyValue:@"key"
+                                                                    primaryKeyType:RLMPropertyTypeString
+                                                                             realm:[RLMRealm defaultRealm]];
     
     XCTAssert(entityChangesObject.addedSafeObjects.count == 0);
+
     [entityChangesObject performSelector:selector withObject:safeObject];
+    
     XCTAssert(entityChangesObject.addedSafeObjects.count == 1);
 }
 
-- (void)testWillDeleteSafeObject {
+- (void)testWillDeleteSafeObject
+{
     RBQEntityChangesObject *entityChangesObject = [self createEntityChangesObject];
+    
     SEL selector = NSSelectorFromString(@"willDeleteSafeObject:");
-    RBQSafeRealmObject *safeObject = [[RBQSafeRealmObject alloc] initWithClassName:@"TestObject" primaryKeyValue:@"key" primaryKeyType:RLMPropertyTypeString realm:[RLMRealm defaultRealm]];
+    
+    RBQSafeRealmObject *safeObject = [[RBQSafeRealmObject alloc] initWithClassName:@"TestObject"
+                                                                   primaryKeyValue:@"key"
+                                                                    primaryKeyType:RLMPropertyTypeString
+                                                                             realm:[RLMRealm defaultRealm]];
     
     XCTAssert(entityChangesObject.deletedSafeObjects.count == 0);
+    
     [entityChangesObject performSelector:selector withObject:safeObject];
+    
     XCTAssert(entityChangesObject.deletedSafeObjects.count == 1);
 }
 
-- (void)testDidChangeSafeObject {
+- (void)testDidChangeSafeObject
+{
     RBQEntityChangesObject *entityChangesObject = [self createEntityChangesObject];
+    
     SEL selector = NSSelectorFromString(@"didChangeSafeObject:");
-    RBQSafeRealmObject *safeObject = [[RBQSafeRealmObject alloc] initWithClassName:@"TestObject" primaryKeyValue:@"key" primaryKeyType:RLMPropertyTypeString realm:[RLMRealm defaultRealm]];
+    
+    RBQSafeRealmObject *safeObject = [[RBQSafeRealmObject alloc] initWithClassName:@"TestObject"
+                                                                   primaryKeyValue:@"key"
+                                                                    primaryKeyType:RLMPropertyTypeString
+                                                                             realm:[RLMRealm defaultRealm]];
     
     XCTAssert(entityChangesObject.changedSafeObjects.count == 0);
+    
     [entityChangesObject performSelector:selector withObject:safeObject];
+    
     XCTAssert(entityChangesObject.changedSafeObjects.count == 1);
 }
+#pragma clang diagnostic pop
 
 @end
