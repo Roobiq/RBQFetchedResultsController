6049379f159137899d4f00af30d58d0d5749f21e
Adam Fish <adam@roobiq.com>
2015-05-29 18:54:13 -0700

    Code formatting clean up.
    Realm style guide emphasizes:
    
    1. The beginning brace should have its own line
    2. Seperate out method contents by functionality to aid visual clarity.
    
    https://github.com/realm/realm-cocoa/wiki/Objective-C-Style-Guide

--- RBQFetchedResultsControllerExample/RBQFetchedResultsControllerExampleTests/RBQFetchedResultsSectionInfoTests.m
+++ RBQFetchedResultsControllerExample/RBQFetchedResultsControllerExampleTests/RBQFetchedResultsSectionInfoTests.m
@@ -18,62 +18,84 @@
 
 #pragma mark - Setup
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
     NSArray *writablePaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsPath = [writablePaths lastObject];
     NSString *testRealmFile = [documentsPath stringByAppendingPathComponent:@"test.realm"];
+    
     [RLMRealm setDefaultRealmPath:testRealmFile];
 
-    [[RLMRealm defaultRealm] transactionWithBlock:^{
-        [[RLMRealm defaultRealm] deleteAllObjects];
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    
+    [realm transactionWithBlock:^{
+        [realm deleteAllObjects];
         
         for (int i=0; i < 10; i++) {
+            
             TestObject *testObject = [[TestObject alloc] init];
             testObject.key = [NSString stringWithFormat:@"key%d", i];
             testObject.title = @"title";
             testObject.inTable = YES;
             testObject.sortIndex = i;
+            
             if (i % 2 == 0) {
                 testObject.sectionName = @"section 1";
             } else {
                 testObject.sectionName = @"section 2";
             }
-            [[RLMRealm defaultRealm] addObject:testObject];
+            
+            [realm addObject:testObject];
         }
     }];
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-    [[RLMRealm defaultRealm] transactionWithBlock:^{
-        [[RLMRealm defaultRealm] deleteAllObjects];
+    
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    
+    [realm transactionWithBlock:^{
+        
+        [realm deleteAllObjects];
     }];
 }
 
 #pragma mark - Utility 
 
-- (RBQFetchedResultsSectionInfo *)createFetchedResultsSectionInfo {
+- (RBQFetchedResultsSectionInfo *)createFetchedResultsSectionInfo
+{
     SEL selector = NSSelectorFromString(@"createSectionWithName:sectionNameKeyPath:fetchRequest:");
+    
     NSMethodSignature *signature = [[RBQFetchedResultsSectionInfo class] methodSignatureForSelector:selector];
     
     NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    
     [invocation setSelector:selector];
     [invocation setTarget:[RBQFetchedResultsSectionInfo class]];
     
     NSString *sectionName = @"section 1";
+    
     NSString *sectionNameKeyPath = @"sectionName";
+    
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"inTable = YES"];
-    RBQFetchRequest *fetchRequest = [RBQFetchRequest fetchRequestWithEntityName:@"TestObject" inRealm:[RLMRealm defaultRealm] predicate:predicate];
+    
+    RBQFetchRequest *fetchRequest = [RBQFetchRequest fetchRequestWithEntityName:@"TestObject"
+                                                                        inRealm:[RLMRealm defaultRealm]
+                                                                      predicate:predicate];
     
     [invocation setArgument:&sectionName atIndex:2];
     [invocation setArgument:&sectionNameKeyPath atIndex:3];
     [invocation setArgument:&fetchRequest atIndex:4];
+    
     [invocation invoke];
     
     RBQFetchedResultsSectionInfo __unsafe_unretained *sectionInfo;
+    
     [invocation getReturnValue:&sectionInfo];
     
     return sectionInfo;
@@ -81,19 +103,25 @@
 
 #pragma mark - Test Case
 
-- (void)testInitializeFetchedResultsSectionInfo {
+- (void)testInitializeFetchedResultsSectionInfo
+{
     RBQFetchedResultsSectionInfo *sectionInfo = [self createFetchedResultsSectionInfo];
+    
     XCTAssertNotNil(sectionInfo);
     XCTAssert([sectionInfo isKindOfClass:[RBQFetchedResultsSectionInfo class]]);
 }
 
-- (void)testObjectsProperty {
+- (void)testObjectsProperty
+{
     RBQFetchedResultsSectionInfo *sectionInfo = [self createFetchedResultsSectionInfo];
+    
     RLMResults *results = sectionInfo.objects;
     
     XCTAssert(sectionInfo.numberOfObjects == 5);
     XCTAssert(results.count == 5);
+    
     for (TestObject *testObject in results) {
+        
         XCTAssert([testObject.sectionName isEqualToString:@"section 1"]);
         XCTAssert(testObject.inTable == YES);
     }
