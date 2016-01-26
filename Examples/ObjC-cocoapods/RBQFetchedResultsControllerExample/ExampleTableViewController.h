c6b9732dbf3ab67e2e6df922fc3e08e8da911fa0
Adam Fish <af@realm.io>
2016-01-25 16:58:21 -0800

    Added two more example apps to show the installation with Cocoapods (pod install is via development pod though so testing can be easier)
    Fixed the podspec so everything installs correctly.

---
+++ Examples/ObjC-cocoapods/RBQFetchedResultsControllerExample/ExampleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExampleTableViewController.h
+//  RBQFetchedResultsControllerExample
+//
+//  Created by Adam Fish on 1/5/15.
+//  Copyright (c) 2015 Roobiq. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ExampleTableViewController : UITableViewController
+
+@end
---
+++ RBQFetchedResultsController/Source/Swift/RBQFRCSwift-BridgingHeader.h
@@ -0,0 +1,9 @@
+//
+//  RBQFRCSwift-BridgingHeader.h
+//  RBQFRCSwiftExample
+//
+//  Created by Adam Fish on 7/23/15.
+//  Copyright (c) 2015 Adam Fish. All rights reserved.
+//
+
+@import RBQFetchedResultsController;
